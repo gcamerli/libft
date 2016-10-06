0c9d9360f02e51065faac5a253addd57f878bf66
Gius. Camerlingo <g.camerlingo@protonmail.ch>
2016-10-06 10:10:26 +0200

    Update functions

--- ft_putstr.c
+++ ft_putstr.c
@@ -5,7 +5,7 @@ char	ft_putstr(char *str)
 	int i;
 	
 	i = 0;
-	while (str[i] >= 32 && str[i] <= 122 && str[i] != '\0')
+	while (str[i] >= 32 && str[i] <= 126 && str[i] != '\0')
 	{
 		ft_putchar(str[i]);
 		i++;
--- ft_strrev.c
+++ ft_strrev.c
@@ -5,7 +5,7 @@ char	ft_strrev(char *str)
 	int i;
 
 	i = 0;
-	while (str[i] >= 32 && str[i] <= 122 && str[i] != '\0')
+	while (str[i] >= 32 && str[i] <= 126 && str[i] != '\0')
 		i++;
 	while (i >= 0)
 	{	
